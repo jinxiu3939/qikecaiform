79fb9620fb86adda7c2e40deb0835ee7b52164f7
hanscai <jinxiu3939@sina.com>
2022-10-19 17:47:06 +0800

    release 0.1.3

--- src/driver/food/component/popuptree/PopupTree.php
+++ src/driver/food/component/popuptree/PopupTree.php
@@ -12,12 +12,14 @@ class PopupTree extends Component
     protected $attributeNames = [
         'mode', // 加载方式，'async' | 'sync'
         'endpoint', // 数据接口
+        'filter', // 是否检索
+        'readonly', // 是否只读
         'size' // 尺寸
     ];
 
     protected $dataNames = [
-        'searchParameter', // 检索条件 [todo]
-        'tree', // 树结构
+        'searchParameter', // 检索条件
         'text', // 关联内容显示文本
+        'tree', // 树结构
     ];
 }
