1cf351d518062692f1ab8a832f7169ec28c4c689
jinxiu3939 <jinxiu3939@sina.com>
2023-12-21 13:52:05 +0800

    release 1.2.0

--- src/driver/food/component/checkbox/Checkbox.php
+++ src/driver/food/component/checkbox/Checkbox.php
@@ -10,6 +10,7 @@ use Qikecai\Sffrender\driver\food\component\radio\Radio;
 class Checkbox extends Radio
 {
     protected $attributeNames = [
+        'all', // 是否显示全选
         'clear', // 是否清除无效的值
         'drag', // 是否可拖动
         'readonly', // 是否只读
